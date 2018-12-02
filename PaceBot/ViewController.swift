536904d412d599357a81e667fa400968815b66d3
Alec Rodgers <alecr1997@gmail.com>
2018-12-01 21:35:18 -0500

    Initial Commit

---
+++ PaceBot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaceBot
+//
+//  Created by Alec Rodgers on 12/1/18.
+//  Copyright © 2018 Alec Rodgers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
