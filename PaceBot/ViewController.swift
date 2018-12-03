1cf82890cc052003937cbffa26503d42be492ea2
Alec Rodgers <alecr1997@gmail.com>
2018-12-03 00:35:02 -0500

    Initial commit

--- PaceBot/ViewController.swift
+++ PaceBot/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func didTapStartButton(_ sender: Any) {
+        
+        let url = URL(string: "http://127.0.0.1:5000/start")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard error == nil else {
+                print(error.debugDescription)
+                return
+            }
+            let responseString = String(data: data!, encoding: String.Encoding.utf8)
+            if let responseString = responseString {
+                print(responseString)
+            }
+        }
+        task.resume()
+    }
+    
+    @IBAction func didTapStopButton(_ sender: Any) {
+        let url = URL(string: "http://127.0.0.1:5000/stop")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard error == nil else {
+                print(error.debugDescription)
+                return
+            }
+            let responseString = String(data: data!, encoding: String.Encoding.utf8)
+            if let responseString = responseString {
+                print(responseString)
+            }
+        }
+        task.resume()
+    }
 }
 
